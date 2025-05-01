08545180091daa0a6be0bd02b88e270d765a49f0
abuomar16 <mohamedabuomar16>
2025-05-01 19:33:13 +0300

    add Allure Report

--- src/test/java/tests/AddationTest.java
+++ src/test/java/tests/AddationTest.java
@@ -1,12 +1,19 @@
 package tests;
 
 import base.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import screens.AdditionScreen;
 
 public class AddationTest extends BaseTest {
 
+
+    @Epic("Additions Test  Module")
+    @Feature("sum 2 number ")
+    @Story(" number 1 is 5  , number 2 is ")
     @Test
     public void testAddition() {
         AdditionScreen calculatorPage = new AdditionScreen(driver);
